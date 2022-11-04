625a380c30b8fae8e1c9bc258d7ee0bb5c6c3d39
Debargha-Mitra-Roy <developer.debarghamitraroy@gmail.com>
2022-11-04 11:26:48 +0530

    Fourty-First Commit

---
+++ 345. Reverse Vowels of a String.cpp
@@ -0,0 +1,54 @@
+/*
+    345. Reverse Vowels of a String :-
+
+    LINK:   https://leetcode.com/problems/reverse-vowels-of-a-string/
+*/
+
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+private:
+    void swap(char *a, char *b)
+    {
+        char temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    // Return true if the character is a vowel (case-insensitive)
+    bool isVowel(char c)
+    {
+        return (c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u' || c == 'A' || c == 'I' || c == 'E' || c == 'O' || c == 'U');
+    }
+
+public:
+    string reverseVowels(string s)
+    {
+        int start = 0;
+        int end = s.size() - 1;
+
+        // While we still have characters to traverse
+        while (start < end)
+        {
+            // Find the leftmost vowel
+            while (start < s.size() && !isVowel(s[start]))
+            {
+                start++;
+            }
+
+            // Find the rightmost vowel
+            while (end >= 0 && !isVowel(s[end]))
+            {
+                end--;
+            }
+
+            // Swap them if start is left of end
+            if (start < end)
+                swap(&s[start++], &s[end--]);
+        }
+
+        return s;
+    }
+};
