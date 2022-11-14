8822cb22f0f72bbce5417ab8b375c54f14ad85a6
Debargha-Mitra-Roy <developer.debarghamitraroy@gmail.com>
2022-11-14 20:21:39 +0530

    Fourty-Ninth Commit

---
+++ 947. Most Stones Removed with Same Row or Column.cpp
@@ -0,0 +1,59 @@
+/*
+    947. Most Stones Removed with Same Row or Column :-
+
+    LINK:   https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+private:
+    bool isValid(vector<int> &pos1, vector<int> &pos2)
+    {
+        if (pos1[0] == pos2[0])
+            return true;
+
+        if (pos1[1] == pos2[1])
+            return true;
+
+        return false;
+    }
+
+    void dfs(vector<vector<int>> &stones, vector<bool> &visited, int currIdx)
+    {
+        visited[currIdx] = true;
+
+        for (int i = 0; i < stones.size(); i++)
+        {
+            if (visited[i])
+                continue;
+
+            if (isValid(stones[i], stones[currIdx]))
+                dfs(stones, visited, i);
+        }
+    }
+
+public:
+    int removeStones(vector<vector<int>> &stones)
+    {
+        int n = stones.size();
+
+        vector<bool> visited(n, false);
+
+        int componentCount = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[i])
+                continue;
+
+            dfs(stones, visited, i);
+            componentCount++;
+        }
+
+        return (n - componentCount);
+    }
+};
